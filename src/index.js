3c510ed640ae776d4a722aeee08269bb8917182c
Frederick Lombar <frederickfiverr@gmail.com>
2021-05-10 10:40:58 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import PrimeraApp from './PrimeraApp'
-import CounterApp from "./CounterApp";
 
-import "./index.css";
+const saludo = <h1>Hola Mundo</h1>;
 
 const divRoot = document.querySelector("#app");
 
-ReactDOM.render(<CounterApp value={10} />, divRoot);
+ReactDOM.render(saludo, divRoot);
